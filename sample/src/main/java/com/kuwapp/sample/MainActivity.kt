4a99316424f5c3cc9f884285605b642cccefa68b
kuwapp <yusuke.hata.dev@gmail.com>
2018-10-04 18:47:37 +0900

    Add login sample

--- sample/src/main/java/com/kuwapp/sample/MainActivity.kt
+++ sample/src/main/java/com/kuwapp/sample/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.kuwapp.sample
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.kuwapp.twitcasting_android.authorize.TwitCastingAuthorizationActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        TwitCastingAuthorizationActivity.startActivity(this)
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        TwitCastingAuthorizationActivity.handelOnActivityResult(requestCode, resultCode) { isAllowed ->
+            Toast.makeText(this, "Authorization ${if (isAllowed) "success" else "failure"}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
---
+++ sample/src/main/java/com/kuwapp/sample/SampleApplication.kt
@@ -0,0 +1,12 @@
+package com.kuwapp.sample
+
+import android.app.Application
+import com.kuwapp.twitcasting_android.TwitCasting
+
+class SampleApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        TwitCasting.initialize(BuildConfig.CLIENT_ID, BuildConfig.CLIENT_SECRET)
+    }
+}
